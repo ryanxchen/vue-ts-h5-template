9ff673edd07f8079d42279a8442cee6e0a16268a
Ryanx Chen <jsryanx@gmail.com>
2019-12-24 23:22:35 +0800

    feat: add not found page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div id="nav" v-if="showNavHandle()" >
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
@@ -20,32 +20,31 @@ Component.registerHooks([
 
 @Component
 export default class App extends Vue {
+  routeName = ''
+
   @Watch('$route')
   private changeRouter (route: Route) {
     console.log(route)
+    this.routeName = route.name || ''
+  }
+
+  private showNavHandle () {
+    return !([
+      'not-found'
+    ].includes(this.routeName))
   }
 }
 </script>
 
 
-<style lang="scss">
-@import '@/assets/styles/global.sass';
+<style lang="sass">
+@import '@/assets/styles/global.sass'
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: $theme-color;
-    }
-  }
-}
+#nav
+  padding: 30px
+  a
+    font-weight: bold
+    color: #2c3e50
+    &.router-link-exact-active 
+      color: $theme-color
 </style>
---
+++ src/components/NotFound/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="not-found">
+    <div class="content">
+      <h1>404</h1>
+      <blockquote>{{ getMsg() }}</blockquote>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+const msgs = [
+  `There's nothing here.`,
+  `How did we get here?`,
+  `That's a Four-Oh-Four.`,
+  `Looks like we've got some broken links.`
+]
+
+@Component
+export default class extends Vue {
+  private getMsg () {
+    return msgs[Math.floor(Math.random() * msgs.length)]
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.content
+  padding-top: 40px
+</style>
+
